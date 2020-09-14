ec778bc559968ea7b8c813ab62a12fd0507ed32d
JeffLiu0076 <51146524+JeffLiu0076@users.noreply.github.com>
2020-09-15 09:24:41 +1000

    Update test-file.c

--- test-file.c
+++ test-file.c
@@ -1,4 +1,12 @@
-void Test()
+void Test(int value)
 {
+   int i = 0;
+   int sum = 0;
+   
+   for (i = 0; i < value; i ++)
+   {
+      sum += i;       
+   }
+   printf("Sum is %u", sum);   
    
 }
